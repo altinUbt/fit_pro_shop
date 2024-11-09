3617aaa970d6dc576c7d44780973c151ba504199
altinUBT <ah70313@ubt-uni.net>
2024-11-09 17:36:32 +0100

    remember me implemented

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 "use strict";
 
 const btn = document.querySelector(".button");
+const rememberMeCheck = document.querySelector("#rememberMe");
+
+// const saveData =
+function saveUserData(email, password) {
+  localStorage.setItem("userEmail", email);
+  localStorage.setItem("userPassword", password);
+}
+
+window.addEventListener("load", function () {
+  const savedEmail = localStorage.getItem("userEmail");
+  const savedPassword = localStorage.getItem("userPassword");
+
+  if (rememberMeCheck.checked) {
+    document.getElementById("email").value = savedEmail;
+    document.getElementById("password").value = savedPassword;
+    rememberMeCheck.checked = true;
+  }
+  if (savedEmail && savedPassword) {
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+  }
+});
 
 const account = {
   email: "ah70313@gmail.com",
@@ -17,8 +39,17 @@ btn.addEventListener("click", function (event) {
     password.length >= 8
   ) {
     console.log("clicked");
-    window.location.href = "login.html";
+    // window.location.href = "";
+
+    if (rememberMeCheck.checked) {
+      saveUserData(email, password);
+    } else {
+      localStorage.removeItem("userEmail");
+      localStorage.removeItem("userPassword");
+    }
   } else {
     console.log("Try again");
   }
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
 });
