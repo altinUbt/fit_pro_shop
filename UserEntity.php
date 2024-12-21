f71badbe1831c979933c751048df01851640170d
altinUBT <ah70313@ubt-uni.net>
2024-12-21 20:56:27 +0100

    User model

---
+++ UserEntity.php
@@ -0,0 +1,41 @@
+<?php
+
+class UserEntity
+{
+    private $id;
+    private $username;
+    private $email;
+    private $password;
+
+    function __construct($id, $username, $email, $password)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+    function getUsername()
+    {
+        return $this->username;
+    }
+    function getEmail()
+    {
+        return $this->email;
+    }
+    function getPassword()
+    {
+        return $this->password;
+    }
+
+    function __tostring()
+    {
+        return "User: " . $this->username . " - " . $this->email;
+    }
+}
+
+?>
