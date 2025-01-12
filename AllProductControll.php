5669a83968f660025acd871d4fd42fbeda9507b9
altinUBT <ah70313@ubt-uni.net>
2025-01-12 23:47:34 +0100

    Controlling errors for product and contolling them

---
+++ AllProductControll.php
@@ -0,0 +1,54 @@
+<?php
+
+include_once 'DBProduct.php';
+include_once 'AllProductEntity.php';
+
+class ProductControll
+{
+    private $products = [];
+    private $errorMessage;
+    private $succedMessage;
+
+    public function __construct()
+    {
+        $this->products = [];
+        $this->errorMessage = "";
+        $this->succedMessage = "";
+    }
+    public function loadPurchase()
+    {
+        $productDBHandler = new DBProduct();
+        $this->products = $productDBHandler->getAllProducts();
+    }
+    public function handlePurchase()
+    {
+        if (isset($_POST['butNowBtn'])) {
+            $productId = $_POST['productId'];
+
+            $productDBHandler = new DBProduct();
+            $product = $productDBHandler->getProductById($productId);
+
+            if ($product) {
+                // $_SESSION['cart'][] = $product; //Pas shtimit te cart.php page
+                $this->succedMessage = "Product purchased successfuly";
+            } else {
+                $this->errorMessage = "Product not found";
+            }
+        }
+    }
+
+    public function getProducts()
+    {
+        return $this->products;
+    }
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+    public function getSuccessMessage()
+    {
+        return $this->succedMessage;
+    }
+}
+
+?>
