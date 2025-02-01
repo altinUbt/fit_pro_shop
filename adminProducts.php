e7a67cef1d0cb3cbb9664bde00ebf4a3a1460880
altinUBT <ah70313@ubt-uni.net>
2025-02-01 14:26:31 +0100

    Created css file for each file and changed path of css

--- adminProducts.php
+++ adminProducts.php
@@ -42,7 +42,7 @@ $successMessage = $productController->getSuccedMessage();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/adminProducts.css">
     <title>Admin Products</title>
 </head>
 
